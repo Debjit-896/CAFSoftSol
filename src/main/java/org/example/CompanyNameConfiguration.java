e6a52b77731916b009ce706618fe7057b538768c
Debjit-896 <debjitpal46@gmail.com>
2026-01-06 23:19:33 +0530

    Code optimization

--- src/main/java/org/example/CompanyNameConfiguration.java
+++ src/main/java/org/example/CompanyNameConfiguration.java
@@ -4,18 +4,8 @@ public class CompanyNameConfiguration {
     public static String configureCompanyName (String name) {
 
         // NULL input handling
-        if (name == null) {
-            return "Company name cannot be null!";
-        }
-
-        //  Empty string handling
-        if (name.isEmpty()) {
-            return "Company name cannot be empty!";
-        }
-
-        // Whitespace handling
-        if (name.trim().isEmpty()) {
-            return "Company name contains only spaces!";
+        if (name == null || name.isEmpty()) {
+            return "Company name cannot be null or empty!";
         }
 
         //  trim and collapse multiple spaces
@@ -23,9 +13,18 @@ public class CompanyNameConfiguration {
                 .trim()
                 .replaceAll("\\s+", " ");
 
-        return switch (value) {
-            case "caf softsol", "caf solution", "caf softsolution india pvt limited" -> "CAF SoftSol India Pvt Ltd.";
-            default -> "Invalid Company Name!";
-        };
+//        return switch (value) {
+//            case "caf softsol", "caf solution", "caf softsolution india pvt limited" -> "CAF SoftSol India Pvt Ltd.";
+//            default -> "Invalid Company Name!";
+
+        switch (value) {
+            case "caf softsol":
+            case "caf solution":
+            case "caf softsolution india pvt limited":
+                return "CAF SoftSol India Pvt Ltd.";
+
+            default:
+                return "Invalid Company Name!";
+        }
     }
 }
